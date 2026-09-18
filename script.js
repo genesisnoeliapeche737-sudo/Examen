f911b21082edde669d48113451ef419797ca13bc
genesisnoeliapeche737-sudo <genesisnoeliapeche737@gmail.com>
2026-09-18 15:06:38 -0300

    Eliminar JavaScript no utilizado

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('action-btn').addEventListener('click', function() {
-    const infoText = document.getElementById('info-text');
-    infoText.textContent = '¡El texto ha cambiado gracias a JavaScript!';
-    infoText.style.fontWeight = 'bold';
-    infoText.style.color = '#007bff';
-});
